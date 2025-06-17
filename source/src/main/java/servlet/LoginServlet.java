abcb6bb96fabe5ba5aed754fe0ec9b5084095398
plusdojo2025-kato <kato-yurika-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-17 15:15:49 +0900

    common.cssとlogin関係

---
+++ source/src/main/java/servlet/LoginServlet.java
@@ -0,0 +1,72 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// ログインページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+			dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		// リクエストパラメータを取得する
+			request.setCharacterEncoding("UTF-8");
+			String id = request.getParameter("id");
+			String pw = request.getParameter("pw");
+			
+			System.out.print(id);
+			System.out.print(pw);
+			
+			
+			// ログイン処理を行う
+//			IdPwDAO iDao = new IdPwDAO();
+//			if (iDao.isLoginOK(new IdPw(id, pw))) { // ログイン成功
+//				// セッションスコープにIDを格納する
+//				HttpSession session = request.getSession();
+//				session.setAttribute("id", new LoginUser(id));
+//
+//				// メニューサーブレットにリダイレクトする
+//				response.sendRedirect("/webapp/MenuServlet");
+//			} else { // ログイン失敗
+//				// リクエストスコープに、タイトル、メッセージ、戻り先を格納する
+//				request.setAttribute("result", new Result("ログイン失敗！", "IDまたはPWに間違いがあります。", "/webapp/LoginServlet"));
+//
+//				// 結果ページにフォワードする
+//				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
+//				dispatcher.forward(request, response);
+//			}
+			
+		doGet(request, response);
+	}
+
+}
