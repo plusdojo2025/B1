d6f394432c1d77a54872797528b9a86876510590
plusdojo2025-kato <kato-yurika-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-19 17:44:31 +0900

    6/19加藤

--- source/src/main/java/servlet/UserManageServlet.java
+++ source/src/main/java/servlet/UserManageServlet.java
@@ -1,6 +1,9 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.Timestamp;    // java.sql.Timestamp
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -10,6 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.SchedulesDao;
 import dao.TasksDao;
 import dto.TasksDto;
 
@@ -57,12 +61,46 @@ public class UserManageServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		
+		//ユーザーID
+		String userIdStr = request.getParameter("partTimeUserId");
+		int userId = Integer.parseInt(userIdStr);
+		
+		//日付
+		String workdayStr = request.getParameter("workday");
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date parsedDate = null;
+		try {
+	        parsedDate = sdf.parse(workdayStr);
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+		// Date → Timestampに変換
+		Timestamp workdayTimestamp = new Timestamp(parsedDate.getTime());
+		
+		
 		// フォームから送られてきた業務IDを受け取る
 		String work1[] = request.getParameterValues("work1");
 		String work2[] = request.getParameterValues("work2");
 		String work3[] = request.getParameterValues("work3");
 		
-		//daoに送る
+		//dao
+		SchedulesDao dao = new SchedulesDao();
+		
+		
+		for (String categoryIdStr : work1) {
+	        int categoryId = Integer.parseInt(categoryIdStr);
+	        dao.insert(userId, categoryId, workdayTimestamp);
+	    }
+		for (String categoryIdStr : work2) {
+	        int categoryId = Integer.parseInt(categoryIdStr);
+	        dao.insert(userId, categoryId, workdayTimestamp);
+	    }
+		for (String categoryIdStr : work3) {
+	        int categoryId = Integer.parseInt(categoryIdStr);
+	        dao.insert(userId, categoryId, workdayTimestamp);
+	    }
+		response.sendRedirect("UserManageServlet");
 		
 		}
 	}
