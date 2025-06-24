27a2d27f3ec9d41d5ea53a0ddcf13085bdf9a822
plusdojo2025-tanada <tanada-rui-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-24 11:17:37 +0900

    一覧画面調整

--- source/src/main/webapp/js/manulist.js
+++ source/src/main/webapp/js/manulist.js
@@ -1,9 +1,13 @@
 // manual.js
 function toggleManual(id) {
-    var el = document.getElementById(id);
-    if (el.style.display === "block") {
-        el.style.display = "none";
-    } else {
-        el.style.display = "block";
-    }
+  const el = document.getElementById(id);
+  const isVisible = el.style.display === "block";
+
+  if (isVisible) {
+    el.style.display = "none";
+    el.classList.remove("active");
+  } else {
+    el.style.display = "block";
+    el.classList.add("active");
+  }
 }
